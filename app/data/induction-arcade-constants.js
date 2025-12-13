ffaf77b11ba486fe145ec9defc15647ed4e0bb09
hypnosis-games <w.macfarl+hypno@gmail.com>
2025-12-13 13:35:24 -0500

    Use continuous depth for induction pacing

--- app/data/induction-arcade-constants.js
+++ app/data/induction-arcade-constants.js
@@ -19,7 +19,9 @@ export const GAME_IDS = Object.freeze({
 });
 
 export const BASE_SPIRAL_INTENSITY = 0.01;
-export const DEPTH_INCREMENT_PER_SUCCESS = .1;
+export const DEPTH_INCREMENT_PER_SUCCESS = 0.1;
+export const DEPTH_INCREMENT_PER_POSITIVE_SURVEY = 0.05;
+export const DEPTH_INCREMENT_PER_RELAXATION_MS = 0.00002;
 export const MAX_DEPTH_LEVEL = 5;
 export const SPIRAL_SCALE_PER_DEPTH = 0.1;
 export const BEAT_INTENSITY_BASE = 0.3;
